e0a32882c34d3433b30e2dee5e1effd7a3f2ffff
Rawal Khirodkar <rawalkhirodkar@gmail.com>
2023-04-28 21:42:26 -0400

    Final commit

--- ego4d/internal/human_pose/scripts/process/3_refine_pose3d.sh
+++ ego4d/internal/human_pose/scripts/process/3_refine_pose3d.sh
@@ -3,8 +3,8 @@ cd ../..
 ##--------------------------------------------------------------
 RUN_FILE=ego4d/internal/human_pose/main.py
 
-CONFIG=unc_T1_rawal; DEVICES=0,
-# CONFIG=iu_bike_rawal; DEVICES=0,
+# CONFIG=unc_T1_rawal; DEVICES=0,
+CONFIG=iu_bike_rawal; DEVICES=0,
 # CONFIG=iu_music_rawal; DEVICES=0,
 # CONFIG=cmu_soccer_rawal; DEVICES=0,
 
