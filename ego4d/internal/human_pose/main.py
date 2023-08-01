7a9405176586b6411966c38ac4d265833528da16
Jinxu Zhang <jinxuzh@seas.upenn.edu>
2023-08-01 17:30:20 -0400

    Comment out legacy code for aria_camera_model

--- ego4d/internal/human_pose/main.py
+++ ego4d/internal/human_pose/main.py
@@ -100,17 +100,13 @@ class aria_camera_model:
         self.camera_model = camera_model
 
     def image_to_world(self, point_2d):
-        try:
-            return self.camera_model.projectionModel.unproject(point_2d)[:2]
-        except AttributeError:
-            return self.camera_model.unproject_no_checks(point_2d)[:2]
+        # return self.camera_model.projectionModel.unproject(point_2d)[:2] # Legacy aria model
+        return self.camera_model.unproject_no_checks(point_2d)[:2]
 
     def world_to_image(self, pt):
         pt_padded = np.append(pt, 1)
-        try:
-            return self.camera_model.projectionModel.project(pt_padded)
-        except AttributeError:
-            return self.camera_model.project_no_checks(pt_padded)
+        # return self.camera_model.projectionModel.project(pt_padded) # Legacy aria model
+        return self.camera_model.project_no_checks(pt_padded)
 
 
 def _create_json_from_capture_dir(capture_dir: Optional[str]) -> Dict[str, Any]:
