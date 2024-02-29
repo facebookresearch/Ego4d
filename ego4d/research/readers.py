1f80aed4308c30fd8a98088e37899186a8d382fa
Miguel Martin <miguelmartin@meta.com>
2024-02-29 04:07:26 +0000

    polish readers

--- ego4d/research/readers.py
+++ ego4d/research/readers.py
@@ -4,14 +4,16 @@ from typing import Optional
 import av
 
 import torch
+from pytorchvideo.transforms import Normalize, ShortSideScale
 from torchaudio.io import StreamReader
-from torchvision.transforms import Resize
+from torchvision.transforms._transforms_video import CenterCropVideo
 
 
 def get_video_meta(path):
     with av.open(path) as cont:
         n_frames = cont.streams[0].frames
         codec = cont.streams[0].codec.name
+        codec_long_name = cont.streams[0].codec.long_name
         tb = cont.streams[0].time_base
 
         all_pts = []
@@ -24,11 +26,12 @@ def get_video_meta(path):
         return {
             "all_pts": sorted(all_pts),
             "codec": codec,
+            "codec_long_name": codec_long_name,
             "tb": tb,
         }
 
 
-def _yuv_to_rgb(img):
+def _yuv_to_rgb(img: torch.Tensor) -> torch.Tensor:
     img = img.to(torch.float)
     y = img[..., 0, :, :]
     u = img[..., 1, :, :]
@@ -43,8 +46,7 @@ def _yuv_to_rgb(img):
     b = y + 2.029 * u
 
     rgb = torch.stack([r, g, b], -1)
-    rgb = (rgb * 255).clamp(0, 255).to(torch.uint8)
-    return rgb
+    return rgb.permute(3, 0, 1, 2)
 
 
 class StridedReader:
@@ -71,7 +73,9 @@ class TorchAudioStreamReader(StridedReader):
         self,
         path: str,
         resize: Optional[int],
+        crop: Optional[int],
         mean: Optional[torch.Tensor],
+        std: Optional[torch.Tensor],
         frame_window_size: int,
         stride: int,
         gpu_idx: int,
@@ -79,7 +83,18 @@ class TorchAudioStreamReader(StridedReader):
         super().__init__(path, stride, frame_window_size)
 
         self.mean = mean
-        self.size = resize
+        self.crop = crop
+        self.std = std
+        self.resize = resize
+        self.resize_transform = (
+            ShortSideScale(self.resize) if self.resize is not None else None
+        )
+        self.norm_transform = (
+            Normalize(mean=self.mean, std=self.std) if self.mean is not None else None
+        )
+        self.crop_transform = (
+            CenterCropVideo(self.crop) if self.crop is not None else None
+        )
         self.create_underlying_cont(gpu_idx)
 
     def create_underlying_cont(self, gpu_id):
@@ -87,10 +102,14 @@ class TorchAudioStreamReader(StridedReader):
 
         decoder_basename = self.meta["codec"]
         if self.gpu_id >= 0:
+            # TODO(miguelmartin): this resize may not work as it is:
+            # 1. not shortest side (can fix with -1xside knowing w/h of video)
+            # 2. unsure about the interpolation
             decoder_opt = (
-                {"resize": f"{self.size}x{self.size}", "gpu": f"{gpu_id}"}
-                if self.size is not None
-                else {}
+                # TODO(miguelmartin): this resize is not the shortest side scale
+                {"resize": f"{self.resize}x{self.resize}", "gpu": f"{gpu_id}"}
+                if self.resize is not None
+                else {"gpu": f"{gpu_id}"}
             )
             self.conf = {
                 "decoder": f"{decoder_basename}_cuvid",
@@ -98,22 +117,23 @@ class TorchAudioStreamReader(StridedReader):
                 "decoder_option": decoder_opt,
                 "stream_index": 0,
             }
-            self.resize = None
+            self.resize_transform = None
         else:
             self.conf = {
                 "decoder": decoder_basename,
                 "stream_index": 0,
             }
-            self.resize = (
-                Resize((self.size, self.size)) if self.size is not None else None
+            self.resize_transform = (
+                ShortSideScale(self.resize) if self.resize is not None else None
             )
 
         self.cont = StreamReader(self.path)
         self.cont.add_video_stream(self.frame_window_size, **self.conf)
+        self.cont.fill_buffer()
 
-    def __getitem__(self, idx: int) -> torch.Tensor:
+    def __getitem__(self, idx: int) -> dict:
         frame_i = self.stride * idx
-        frame_j = frame_i + self.frame_window_size
+        frame_j = frame_i + self.frame_window_size - 1
         assert frame_i >= 0 and frame_j < len(self.all_pts)
 
         frame_i_pts = self.all_pts[frame_i]
@@ -125,12 +145,18 @@ class TorchAudioStreamReader(StridedReader):
         assert fs is not None
         assert len(fs) == 1
         ret = _yuv_to_rgb(fs[0])  # TODO: optimize me
-        assert ret.shape[0] == self.frame_window_size
-        if self.resize is not None:
-            ret = self.resize(ret)
-        if self.mean is not None:
-            ret -= self.mean
-        return ret
+        assert ret.shape[1] == self.frame_window_size
+        if self.resize_transform is not None:
+            ret = self.resize_transform(ret)
+        if self.crop_transform is not None:
+            ret = self.crop_transform(ret)
+        if self.norm_transform is not None:
+            ret = self.norm_transform(ret)
+        return {
+            "video": ret,
+            "frame_start_idx": frame_i,
+            "frame_end_idx": frame_j,
+        }
 
 
 class PyAvReader(StridedReader):
@@ -138,7 +164,9 @@ class PyAvReader(StridedReader):
         self,
         path: str,
         resize: Optional[int],
+        crop: Optional[int],
         mean: Optional[torch.Tensor],
+        std: Optional[torch.Tensor],
         frame_window_size: int,
         stride: int,
         gpu_idx: int,
@@ -146,37 +174,58 @@ class PyAvReader(StridedReader):
         super().__init__(path, stride, frame_window_size)
 
         if gpu_idx >= 0:
-            raise AssertionError("GPU decoding not support for pyav")
+            print("WARN: GPU decoding not supported for PyAV, using CPU")
 
         self.mean = mean
-        self.resize = Resize((resize, resize)) if resize is not None else None
+        self.crop = crop
+        self.std = std
+        self.resize = resize
+        self.resize_transform = (
+            ShortSideScale(self.resize) if self.resize is not None else None
+        )
+        self.norm_transform = (
+            Normalize(mean=self.mean, std=self.std) if self.mean is not None else None
+        )
+        self.crop_transform = (
+            CenterCropVideo(self.crop) if self.crop is not None else None
+        )
         self.path = path
         self.create_underlying_cont(gpu_idx)
 
-    def create_underlying_cont(self, gpu_id):
+    def create_underlying_cont(self, _):
         self.cont = av.open(self.path)
 
-    def __getitem__(self, idx: int) -> torch.Tensor:
+    def __getitem__(self, idx: int) -> dict:
         frame_i = self.stride * idx
-        frame_j = frame_i + self.frame_window_size
+        frame_j = frame_i + self.frame_window_size - 1
         assert frame_i >= 0 and frame_j < len(self.all_pts)
 
         frame_i_pts = self.all_pts[frame_i]
         frame_j_pts = self.all_pts[frame_j]
-        # self.cont.streams.video[0].seek(frame_i_pts)
         self.cont.seek(frame_i_pts, stream=self.cont.streams.video[0])
         fs = []
         for f in self.cont.decode(video=0):
-            # print(f.pts)
             if f.pts < frame_i_pts:
                 continue
             if f.pts >= frame_j_pts:
                 break
-            fs.append((f.pts, torch.tensor(f.to_ndarray(format="rgb24"))))
+            fs.append(
+                (
+                    f.pts,
+                    torch.tensor(f.to_ndarray(format="rgb24"), dtype=torch.float32)
+                    / 255,
+                )
+            )
         fs = sorted(fs, key=lambda x: x[0])
-        ret = torch.stack([x[1] for x in fs])
-        if self.resize is not None:
-            ret = self.resize(ret)
-        if self.mean is not None:
-            ret -= self.mean
-        return ret
+        ret = torch.stack([x[1] for x in fs]).permute(3, 0, 1, 2)
+        if self.resize_transform is not None:
+            ret = self.resize_transform(ret)
+        if self.crop_transform is not None:
+            ret = self.crop_transform(ret)
+        if self.norm_transform is not None:
+            ret = self.norm_transform(ret)
+        return {
+            "video": ret,
+            "frame_start_idx": frame_i,
+            "frame_end_idx": frame_j,
+        }
