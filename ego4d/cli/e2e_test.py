fe14c70f5e0fae9c206303b0ab8d09f8d8e84cbc
Caleb Ho <calebh@meta.com>
2024-05-31 13:15:58 -0700

    buckification
    Summary:
    This commit was generated using `mgt import`.
    buckification for third-party libraries:
    third-party/pypi/responses/0.25.0
    third-party/pypi/moto/5.0.8
    
    uuid_e3b367cdbe054c858e46ee4fad2a911b
    
    Reviewed By: ajinkya-ghonge, luccabb
    
    Differential Revision: D57881544
    
    fbshipit-source-id: d4a3ee5963bf0b024d7510687d71a14f31d37c1c

--- ego4d/cli/e2e_test.py
+++ ego4d/cli/e2e_test.py
@@ -1,10 +1,10 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
 
 import boto3
-from moto import mock_s3
+from moto import mock_aws
 
 
-@mock_s3
+@mock_aws
 def test_s3():
     s3 = boto3.resource("s3")
     print(s3)
