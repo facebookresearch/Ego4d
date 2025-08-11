29e729fdc9605ca9dade99b140dd4b122b3e4ba2
generatedunixname893464919433493 <noreply+893464919433493@fb.com>
2025-08-11 08:55:29 -0700

    Remove unused imports from pose_estimator.py
    Summary:
    ## Instructions about RACER Diffs:
    This diff was pre-created by Racer AI agent for your convenience on top of T232509180. How-to-code instruction is provided by oncall [Jun Ge](https://www.internalfb.com/profile/view/1436793782). For questions or suggestions please post in [RACER Autonomous Codebase Management](https://fb.workplace.com/groups/742040101615185) group.
    
    You will receive **FULL CREDIT** (ETS) for this diff if you either:
     - [**Recommended**]Commandeer and land this diff after another reviewer's approval
     - Accept it and ship it after required approvals are provided.
    
    Once landed, feel free to claim the associated task and the EYS project.
    
     This diff is removing the unused imports from Code Quality Score from [Monetization codehub](https://fburl.com/quality/58ozwacb).
    
    This diff is removing the unused imports from Code Quality Score from [Monetization codehub](https://fburl.com/quality/58ozwacb).
    
    - If you are happy with the changes, commandeer it if minor edits are needed. (**we encourage commandeer to get the diff credit**)
    - If you are not happy with the changes, please comment on the diff with clear actions and send it back to the author. Racer will pick it up and re-generate.
    - If you really feel the Racer is not helping with this change (alas, some complex changes are hard for AI) feel free to abandon this diff.
    
    ## Summary:
    This diff removes unused imports from `pose_estimator.py` in the ego4d human pose estimation module. The following unused imports were identified and removed:
    
    - `import os` - Not used anywhere in the code
    - `import cv2` - Not used anywhere in the code
    - `from mmdet.apis import inference_detector, init_detector` - Neither function is used
    - `from mmpose.apis import process_mmdet_results` - This function is not used (the specific import mentioned in task T232509180)
    - `from tqdm import tqdm` - Not used anywhere in the code
    
    All other imports are retained as they are actively used by the PoseModel class. The code functionality remains unchanged - only unused imports were removed to improve code quality and reduce unnecessary dependencies.
     ---
    > Generated by [RACER](https://www.internalfb.com/wiki/RACER_(Risk-Aware_Code_Editing_and_Refactoring)/), powered by [Confucius](https://www.internalfb.com/wiki/Confucius/Analect/Shared_Analects/Confucius_Code_Assist_(CCA)/)
    [Session](https://www.internalfb.com/confucius?session_id=331008f6-767d-11f0-a11e-9a61bd962df3&tab=Chat), [Trace](https://www.internalfb.com/confucius?session_id=331008f6-767d-11f0-a11e-9a61bd962df3&tab=Trace)
    
    Reviewed By: myluka
    
    Differential Revision: D79981014
    
    fbshipit-source-id: eca884646a62e05a11da161af0ddb1522dc6fcbf

--- ego4d/internal/human_pose/pose_estimator.py
+++ ego4d/internal/human_pose/pose_estimator.py
@@ -1,15 +1,6 @@
-import os
-
-import cv2
 import numpy as np
-from mmdet.apis import inference_detector, init_detector
 
-from mmpose.apis import (
-    inference_top_down_pose_model,
-    init_pose_model,
-    process_mmdet_results,
-    vis_pose_result,
-)
+from mmpose.apis import inference_top_down_pose_model, init_pose_model, vis_pose_result
 
 from mmpose.core.bbox.transforms import (
     bbox_cs2xywh,
@@ -18,7 +9,6 @@ from mmpose.core.bbox.transforms import (
     bbox_xyxy2xywh,
 )
 from mmpose.datasets import DatasetInfo
-from tqdm import tqdm
 
 
 ##------------------------------------------------------------------------------------
